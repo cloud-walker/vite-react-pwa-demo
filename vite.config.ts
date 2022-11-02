dd792c031dcd4170099f53e15aa29f4a1f62ad89
Luca Barone <baro.luc@gmail.com>
2022-11-02 20:15:41 +0100

    🚧 Add PWA Minimal Requirements

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,34 @@ import {VitePWA} from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA()],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ['vite.svg'],
+      manifest: {
+        name: 'Vite + React PWA Demo',
+        short_name: 'VPWA',
+        description: 'A description...',
+        theme_color: '#FF0000',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
 })
