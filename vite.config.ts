3c2b50936eeefbf74851abe55400f5a7019099c1
Luca Barone <baro.luc@gmail.com>
2022-11-02 20:49:29 +0100

    🚧 Specify registerType to prompt

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
+      registerType: 'prompt',
       includeAssets: ['vite.svg'],
       manifest: {
         name: 'Vite + React PWA Demo',
